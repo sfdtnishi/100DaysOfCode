873fbe72135594594bb0da4a8ee81082e0ea8abb
Nishi Jain <79569723+sfdtnishi@users.noreply.github.com>
2023-03-08 16:26:06 +0530

    Create infiniteLoop.java
    Using a do-while loop, till the condition of while is true, the loop will iterate and the statements under the "do" will be executed.

---
+++ infiniteLoop.java
@@ -0,0 +1,9 @@
+public class infiniteLoop {
+    public static void main(String args[])
+    {
+        do{
+            System.out.print("infinite loop");
+        }
+        while(false);  //put true here and it will be an infinite loop
+    }
+}
