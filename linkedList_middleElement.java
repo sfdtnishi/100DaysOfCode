37639166125bfb05a64eb02422f35950f4a26566
Nishi Jain <79569723+sfdtnishi@users.noreply.github.com>
2023-04-29 21:50:44 +0530

    Create linkedList_middleElement.java

---
+++ linkedList_middleElement.java
@@ -0,0 +1,10 @@
+public ListNode middleNode(ListNode head) {
+            ListNode middle = head;
+            ListNode end = head;
+
+            while(end!=null && end.next!=null){
+                middle =middle.next;
+                end = end.next.next;
+            }
+            return middle;
+        }
