46ae8febb1833ba8b66a85f80ccb8152b14fa4c1
Nishi Jain <79569723+sfdtnishi@users.noreply.github.com>
2023-04-29 23:21:40 +0530

    Create linkedList_cycle.java

---
+++ linkedList_cycle.java
@@ -0,0 +1,18 @@
+public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        if(head==null){
+            return false;
+        }
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
