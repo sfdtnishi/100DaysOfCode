8af4637445f8db7428e8795cc1b2adbeb81e4556
Nishi Jain <79569723+sfdtnishi@users.noreply.github.com>
2023-06-03 02:54:12 +0530

    Update queue_UsingLinkedList.java

--- queue_UsingLinkedList.java
+++ queue_UsingLinkedList.java
@@ -1,1 +1,72 @@
+package com.queue;
 
+import java.util.*;
+
+class queuee{
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head = null;
+    public static Node tail = null;
+
+    public static boolean isEmpty(){
+        return head==null && tail == null;
+    }
+
+    public void add(int data){
+        Node newNode = new Node(data);
+        if(isEmpty()){
+            tail = head = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public static int remove(){
+        if(isEmpty()){
+            System.out.println("queue is empty");
+            return -1;
+        }
+        int result = head.data;
+        if(head==tail){
+            tail = null;
+        }
+        head = head.next;
+        return result;
+
+
+    }
+
+    public static int peek(){
+        if(isEmpty()){
+            System.out.println("queue is empty");
+            return -1;
+        }
+        return head.data;
+    }
+}
+
+public class circularQueueUsingLinkedList {
+    public static void main(String[] args) {
+        queuee q = new queuee();
+//        Queue<Integer> q = new LinkedList<>();  // using collection framework
+//        Queue<Integer> q = new ArrayDeque<>();   //here same class name is not used because Queue is an interface and interfaces don't have objects. LinkedList and ArrayDeque are the classes which have extended from Queue interface so we can use them.
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+
+
+}
